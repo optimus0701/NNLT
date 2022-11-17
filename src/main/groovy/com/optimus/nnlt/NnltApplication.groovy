5a2d8f18b63548719776ce61028bf160874abb2a
optimus0701 <vido0701@gmail.com>
2022-11-18 00:39:23 +0700

    Initial commit

---
+++ src/main/groovy/com/optimus/nnlt/NnltApplication.groovy
@@ -0,0 +1,13 @@
+package com.optimus.nnlt
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NnltApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NnltApplication, args)
+    }
+
+}
