935d73eb6df9881afa2a6bd02399a0ee59606538
optimus0701 <vido0701@gmail.com>
2022-11-21 16:33:21 +0700

    optimus add h2 database

--- src/main/groovy/com/optimus/nnlt/NnltApplication.groovy
+++ src/main/groovy/com/optimus/nnlt/NnltApplication.groovy
@@ -3,9 +3,9 @@ package com.optimus.nnlt
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+
 @SpringBootApplication
 class NnltApplication {
-
     static void main(String[] args) {
         SpringApplication.run(NnltApplication, args)
     }
